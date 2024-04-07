a8e9d7018ab849e0c7fa62ce03fca9a6464693ae
elkadirizouhra <zahiraelkadiri6@gmail.com>
2024-04-07 00:16:07 +0000

    To-Do-List

---
+++ autoformation/react_vite/my_app/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import Fruit from './fv/fruit.jsx'
+// import ColorPicker from './colorPicker/colorPicker.jsx'
+// import Card from './card/card.jsx'
+// import Button from './button.jsx'
+// import Conter from './counter/conter.jsx'
+// import  Mycomponent from './MycomponenetHook.jsx'
+// import Onchange from './Onchange.jsx'
+// import UpdateObject from './updateObject.jsx'
+// import Food from './updateArray.jsx'
+// import Cars from './listeCar.jsx'
+import Todolist from './todoList/todoList.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <Todolist/>
+  </React.StrictMode>
+)
